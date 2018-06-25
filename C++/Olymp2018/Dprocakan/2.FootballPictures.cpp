ac0a66a6011f01f2cb0b33bd8663a17122187888
Sevan_Shahnazaryan <sevanbbx@gmail.com>
2018-06-25 14:40:02 +0400

    Added 2.FootballPictures.cpp

--- C++/Olymp2018/Dprocakan/2.FootballPictures.cpp
+++ C++/Olymp2018/Dprocakan/2.FootballPictures.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 
-int* numDevisors(int numOfSquares){
-    
-    for(int i = 1; i < amount; ++i){
-        
+int numDevisors(int numOfSquares){
+    int devisors[numOfSquares/4];
+    int j = 0;
+    for(int i = 1; i < numOfSquares / 2 + 1; ++i){
+        if(numOfSquares % i == 0){
+            devisors[j] = (i + i + ((numOfSquares / i) * 2));
+            ++j;
+        }
     }
+    int min = devisors[0];
+    for(int i = 0; i < j; ++i){
+        if(devisors[i] < min && devisors[i] > 4){
+            min = devisors[i];
+        }
+    }
+    return min;
 }
 
 int main(){
     int numOfSquares = 0;
     std::cin >> numOfSquares;
+    std::cout << numDevisors(numOfSquares) << std::endl;
+    return 0;
 }
